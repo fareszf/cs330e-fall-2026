bdc38cac6500882ecd9781837116950b3067339a
Fares Fraij <fares@cs.utexas.edu>
2026-08-24 14:47:04 -0500

    uploading additional material

--- examples/Assertions.py
+++ examples/Assertions.py
@@ -6,39 +6,93 @@
 
 # https://docs.python.org/3/reference/simple_stmts.html#grammar-token-assert-stmt
 
-def cycle_length (n) :
+
+def cycle_length(n):
     assert n > 0
     c = 0
-    while n > 1 :
-        if (n % 2) == 0 :
-            n = (n // 2)
-        else :
+    while n > 1:
+        if (n % 2) == 0:
+            n = n // 2
+        else:
             n = (3 * n) + 1
         c += 1
     assert c > 0
     return c
 
+
 print("Assertions.py")
 
-assert cycle_length( 1) == 1
-assert cycle_length( 5) == 6
+assert cycle_length(1) == 1
+assert cycle_length(5) == 6
 assert cycle_length(10) == 7
 
 print("Done.")
 
-""" #pragma: no cover
-$ python Assertions.py
-Assertions.py
-Traceback (most recent call last):
-  File "Assertions.py", line 23, in <module>
-    assert cycle_length( 1) == 1
-  File "Assertions.py", line 18, in cycle_length
+
+"""
+-----------------------------------------------------------------------------
+Sample terminal session / commands
+-----------------------------------------------------------------------------
+
+This file is named Assertions.py.
+
+This program intentionally contains an assertion that fails.
+
+The function starts with:
+
+    c = 0
+
+When n is 1, the while loop does not run:
+
+    while n > 1:
+
+Therefore, c is still 0 when this assertion is reached:
+
     assert c > 0
-AssertionError
+
+Run the program on UTCS Linux or macOS Terminal:
+
+    $ python3 Assertions.py
+    Assertions.py
+    Traceback (most recent call last):
+      File "/u/fares/.../Assertions.py", line 25, in <module>
+        assert cycle_length(1) == 1
+               ^^^^^^^^^^^^^^^
+      File "/u/fares/.../Assertions.py", line 19, in cycle_length
+        assert c > 0
+               ^^^^^
+    AssertionError
 
 
+The traceback shows two important things:
 
-$ python -O Assertions.py
-Assertions.py
-Done.
+    assert cycle_length(1) == 1
+
+This is the line that called the function.
+
+    assert c > 0
+
+This is the assertion inside the function that actually failed.
+
+Run the program with Python optimization enabled:
+
+    $ python3 -O Assertions.py
+    Assertions.py
+    Done.
+
+The -O option runs Python in optimized mode.
+
+In optimized mode, Python disables assert statements. Therefore, Python skips:
+
+    assert n > 0
+    assert c > 0
+    assert cycle_length(1) == 1
+    assert cycle_length(5) == 6
+    assert cycle_length(10) == 7
+
+Because those assert statements are skipped, the program reaches:
+
+    print("Done.")
+
+-----------------------------------------------------------------------------
 """
--- examples/Hello.py
+++ examples/Hello.py
@@ -4,213 +4,194 @@
 # Hello.py
 # --------
 
-# https://www.python.org
-# https://docs.python.org/release/3.10.11/
-# https://docs.python.org/release/3.10.11/library/
+"""
+A simple Python starter program.
 
-print("Nothing to be done.")
+The UTCS machines currently use Python 3.12.3.
+Students should install Python 3.12.x locally, preferably Python 3.12.3,
+to match the course environment as closely as possible.
 
-""" #pragma: no cover
-Developed in 1989 by Guido van Rossum of the Netherlands, 
-Distinguished Engineer at Microsoft, since November 2020.
-
-Python is procedural, object-oriented, dynamically typed, and garbage collected.
-
-The following commands were executed on UTCS machines (Linux).
---------------------------------------------------------------
-$ python --version
-Python 2.7.18
+The following commands are intended for the UTCS Linux machines.
+They should also work on macOS Terminal, but may not work as written
+on Windows Command Prompt or PowerShell.
+"""
 
 
-
-$ python3 --version
-Python 3.8.10
-
-
-
-$ python3 ./Hello.py
-Nothing to be done.
+def main():
+    """Run the program."""
+    print("Nothing to be done.")
 
 
-
-$ ls -l Hello.py
--rw------- 1 fares prof 5354 Jun 4  2025 Hello.py
-
-$ chmod ugo+x Hello.py
-
-$ Hello.py
-Nothing to be done.
-
-$ ls -l Hello.py
--rwx--x--x 1 fares prof 5354 Jun 4  2025 Hello.py
+if __name__ == "__main__":
+    main()
 
 
+"""
+-----------------------------------------------------------------------------
+Sample terminal session / commands
+-----------------------------------------------------------------------------
 
-$ python3
-Python 3.8.10 (default, Mar 18 2025, 20:04:55)
-[GCC 9.4.0] on linux
-Type "help", "copyright", "credits" or "license" for more information.
->>> import Hello
-Nothing to be done.
+This file is named Hello.py.
 
->>> quit()
+On the UTCS Linux machines, check the Python version:
 
+    $ python3 --version
+    Python 3.12.3
 
+Run the program with python3:
 
-$ python3 -m cProfile Hello.py
-Nothing to be done.
-         4 function calls in 0.000 seconds
+    $ python3 Hello.py
+    Nothing to be done.
 
-   Ordered by: standard name
+Make the program executable:
+
+    $ chmod ugo+x Hello.py
+
+Then run it directly:
+
+    $ ./Hello.py
+    Nothing to be done.
+
+Run the program with the Python profiler:
+
+    $ python3 -m cProfile Hello.py
+    Nothing to be done.
+         5 function calls in 0.000 seconds
+
+   Ordered by: cumulative time
 
    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
-        1    0.000    0.000    0.000    0.000 Hello.py:11(<module>)
         1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
-        1    0.000    0.000    0.000    0.000 {built-in method builtins.print}
+        1    0.000    0.000    0.000    0.000 Hello.py:1(<module>)
+        1    0.000    0.000    0.000    0.000 Hello.py:20(main)
         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
-
-
-		
-$ python3
-Python 3.8.10 (default, Mar 18 2025, 20:04:55)
-[GCC 9.4.0] on linux
-Type "help", "copyright", "credits" or "license" for more information.
->>> help()
-
-Welcome to Python 3.8's help utility!
-
-If this is your first time using Python, you should definitely check out
-the tutorial on the Internet at https://docs.python.org/3.8/tutorial/.
-
-Enter the name of any module, keyword, or topic to get help on writing
-Python programs and using Python modules.  To quit this help utility and
-return to the interpreter, just type "quit".
-
-To get a list of available modules, keywords, symbols, or topics, type
-"modules", "keywords", "symbols", or "topics".  Each module also comes
-with a one-line summary of what it does; to list the modules whose name
-or summary contain a given string such as "spam", type "modules spam".
-
-help>range
-Help on class range in module builtins:
-
-class range(object)
- |  range(stop) -> range object
- |  range(start, stop[, step]) -> range object
- |
- |  Return an object that produces a sequence of integers from start (inclusive)
- |  to stop (exclusive) by step.  range(i, j) produces i, i+1, i+2, ..., j-1.
- |  start defaults to 0, and stop is omitted!  range(4) produces 0, 1, 2, 3.
- |  These are exactly the valid indices for a list of 4 elements.
- |  When step is given, it specifies the increment (or decrement).
- |
- |  Methods defined here:
- |
- |  __bool__(self, /)
- |      self != 0
- |
- |  __contains__(self, key, /)
- |      Return key in self.
- |
- |  __eq__(self, value, /)
- |      Return self==value.
- |
- |  __ge__(self, value, /)
- |      Return self>=value.
- |
- |  __getattribute__(self, name, /)
- |      Return getattr(self, name).
- |
- |  __getitem__(self, key, /)
- |      Return self[key].
- |
- |  __gt__(self, value, /)
- |      Return self>value.
- |
- |  __hash__(self, /)
- |      Return hash(self).
- |
- |  __iter__(self, /)
- |      Implement iter(self).
- |
- |  __le__(self, value, /)
- |      Return self<=value.
- |
- |  __len__(self, /)
- |      Return len(self).
- |
- |  __lt__(self, value, /)
- |      Return self<value.
- |
- |  __ne__(self, value, /)
- |      Return self!=value.
- |
- |  __new__(*args, **kwargs) from builtins.type
- |      Create and return a new object.  See help(type) for accurate signature.
- |
- |  __reduce__(...)
- |      helper for pickle
- |
- |  __repr__(self, /)
- |      Return repr(self).
- |
- |  __reversed__(...)
- |      Return a reverse iterator.
- |
- |  count(...)
- |      rangeobject.count(value) -> integer -- return number of occurrences of value
- |
- |  index(...)
- |      rangeobject.index(value, [start, [stop]]) -> integer -- return index of value.
- |      Raise ValueError if the value is not present.
- |
- |  ----------------------------------------------------------------------
- |  Data descriptors defined here:
- |
- |  start
- |
- |  step
- |
- |  stop
-
-
- 
-help> quit
-
-You are now leaving help and returning to the Python interpreter.
-If you want to ask for help on a particular object directly from the
-interpreter, you can type "help(object)".  Executing "help('string')"
-has the same effect as typing a particular string at the help> prompt.
-
->>> quit()
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.print}
 
 
 
-$ python3
-Python 3.8.10 (default, Mar 18 2025, 20:04:55)
-[GCC 9.4.0] on linux
-Type "help", "copyright", "credits" or "license" for more information.
->>> import this
-The Zen of Python, by Tim Peters
+If python does not work on Windows, try the Python Launcher instead:
 
-Beautiful is better than ugly.
-Explicit is better than implicit.
-Simple is better than complex.
-Complex is better than complicated.
-Flat is better than nested.
-Sparse is better than dense.
-Readability counts.
-Special cases aren't special enough to break the rules.
-Although practicality beats purity.
-Errors should never pass silently.
-Unless explicitly silenced.
-In the face of ambiguity, refuse the temptation to guess.
-There should be one-- and preferably only one --obvious way to do it.
-Although that way may not be obvious at first unless you're Dutch.
-Now is better than never.
-Although never is often better than *right* now.
-If the implementation is hard to explain, it's a bad idea.
-If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!
->>> quit()
+    py --version
+    py Hello.py
+
+The first line of this file,
+
+    #!/usr/bin/env python3
+
+is called a shebang. On Linux/macOS, it tells the operating system to run
+this file using python3 when the file is executed directly.
+
+The block
+
+    if __name__ == "__main__":
+        main()
+
+means: call main() only when this file is run directly as a program.
+
+If this file is imported as a module, main() is not called automatically.
+
+Example:
+
+    $ python3
+    >>> import Hello
+    >>> Hello.main()
+    Nothing to be done.
+    >>> quit()
+
+To get interactive Python help:
+
+    $ python3
+    Python 3.12.3 (main, Mar 23 2026, 19:04:32) [GCC 13.3.0] on linux
+    ...
+    >>> help()
+    Welcome to Python 3.12's help utility! If this is your first time using
+    Python, you should definitely check out the tutorial at
+    https://docs.python.org/3.12/tutorial/.
+    ...
+    To quit this help utility and return to the interpreter,
+enter "q" or "quit".
+
+    help> range
+    Help on class range in module builtins:
+
+    class range(object)
+     |  range(stop) -> range object
+     |  range(start, stop[, step]) -> range object
+     |
+     |  Return an object that produces a sequence of integers from start (inclusive)
+     |  to stop (exclusive) by step.  range(i, j) produces i, i+1, i+2, ..., j-1.
+     |  start defaults to 0, and stop is omitted!  range(4) produces 0, 1, 2, 3.
+     |  These are exactly the valid indices for a list of 4 elements.
+     |  When step is given, it specifies the increment (or decrement).
+     |
+     |  Methods defined here:
+     |
+     |  __bool__(self, /)
+     |      True if self else False
+     |
+     |  __contains__(self, key, /)
+     |      Return bool(key in self).
+     |
+     |  __eq__(self, value, /)
+     |      Return self==value.
+     |
+     |  __ge__(self, value, /)
+     |      Return self>=value.
+     |
+     |  __getattribute__(self, name, /)
+     |      Return getattr(self, name).
+     |
+     |  __getitem__(self, key, /)
+     |      Return self[key].
+     |
+     |  __gt__(self, value, /)
+     |      Return self>value.
+     |
+     |  __hash__(self, /)
+     |      Return hash(self).
+     |
+     |  __iter__(self, /)
+     |      Implement iter(self).
+     |
+     |  __le__(self, value, /)
+     |      Return self<=value.
+     |
+     |  __len__(self, /)
+     |      Return len(self).
+    ...
+    help> quit
+    >>> quit()
+
+To see The Zen of Python:
+
+    $ python3
+    >>> import this
+    The Zen of Python, by Tim Peters
+
+    Beautiful is better than ugly.
+    Explicit is better than implicit.
+    Simple is better than complex.
+    Complex is better than complicated.
+    Flat is better than nested.
+    Sparse is better than dense.
+    Readability counts.
+    Special cases aren't special enough to break the rules.
+    Although practicality beats purity.
+    Errors should never pass silently.
+    Unless explicitly silenced.
+    In the face of ambiguity, refuse the temptation to guess.
+    There should be one-- and preferably only one --obvious way to do it.
+    Although that way may not be obvious at first unless you're Dutch.
+    Now is better than never.
+    Although never is often better than *right* now.
+    If the implementation is hard to explain, it's a bad idea.
+    If the implementation is easy to explain, it may be a good idea.
+    Namespaces are one honking great idea -- let's do more of those!
+
+    >>> quit()
+
+Python was created by Guido van Rossum and first released in 1991.
+
+Python is procedural, object-oriented, dynamically typed, and garbage collected.
+-----------------------------------------------------------------------------
 """
